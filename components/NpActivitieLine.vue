bbe0bc5b9cbc04a6bf5dacd954fd946e266d6015
mkobayashime <m.kobayashi.me@gmail.com>
2018-12-23 21:11:04 +0900

    Add NpSectionActivities components

---
+++ components/NpActivitieLine.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  .lineContainer
+    .line(v-for="p in ps")
+      .date.fi.fi-up(data-emergence="hidden") {{p.date}}
+      .textContainer
+        .text.fi.fi-up(data-emergence="hidden") {{p.text}}
+        .linkContainer(v-for="link in p.links")
+          a(:href="link[1]" target="_blank" v-if="!link[2]")
+            np-button-s {{link[0]}}
+          nuxt-link(:to="link[1]" v-if="link[2]")
+            np-button-s {{link[0]}}
+</template>
+
+<script>
+import NpButtonS from '~/components/NpButtonS.vue'
+export default {
+  name: 'NpActivitieLine',
+  components: { NpButtonS },
+  props: {
+    ps: {
+      type: Array,
+      default: function() {
+        return {
+          date: 'xx.xx - xx.xx',
+          text: 'text',
+          links: ['link label', 'link', false]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.line
+  display flex
+  margin 30px 0
+  .date
+    width 200px
+    font-size 26px
+    font-weight 700
+  .textContainer
+    display flex
+    flex-wrap wrap
+    width 600px
+    .text
+      width 600px
+      margin-bottom 10px
+      line-height 2em
+  .linkContainer
+    display flex
+</style>
---
+++ components/NpSectionActivities.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .npSectionContainer
+    np-separator
+    h2.fi.fi-up(data-emergence="hidden") {{section.year}}
+    np-activitie-line(:ps="section.ps")
+</template>
+
+<script>
+import emergence from 'emergence.js'
+import NpActivitieLine from '~/components/NpActivitieLine.vue'
+import NpButtonS from '~/components/NpButtonS.vue'
+import NpSeparator from '~/components/NpSeparator.vue'
+export default {
+  name: 'NpSection',
+  components: { NpActivitieLine, NpButtonS, NpSeparator },
+  props: {
+    section: {
+      type: Object,
+      default: function() {
+        return {
+          yaer: '20xx'
+        }
+      }
+    }
+  },
+  computed: {
+    getLink() {
+      return this.section.ps.link
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+h2
+  margin 100px 0 50px
+  font-size 40px
+</style>
