c030b9d93c2e942d110254996e148f5e87d789db
mkobayashime <m.kobayashi.me@gmail.com>
2018-12-23 21:53:37 +0900

    Update layout settings for Sidebar

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -42,7 +42,7 @@ export default {
 
 <style lang="stylus" scoped>
 .theSidebar
-  width 340px
+  width 100%
   height 100%
   display flex
   align-items center
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,41 @@
 <template lang="pug">
-  div
-    nuxt
+  #root
+    the-sidebar#theSidebar
+    #scrollArea
+      nuxt
 </template>
+
+<script>
+import TheSidebar from '~/components/TheSidebar.vue'
+export default {
+  components: { TheSidebar }
+}
+</script>
 
 <style lang="stylus">
 html
-  font-size: 16px;
-  font-family: 'Roboto', 'Noto Sans JP', sans-serif;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  font-size 16px
+  font-family 'Roboto', 'Noto Sans JP', sans-serif
+  -ms-text-size-adjust 100%
+  -webkit-text-size-adjust 100%
+  -moz-osx-font-smoothing grayscale
+  -webkit-font-smoothing antialiased
+  box-sizing border-box
 
 *,
 *:before,
 *:after
-  box-sizing: border-box;
-  margin: 0;
+  box-sizing border-box
+  margin 0
+
+#theSidebar
+  width 340px
+  height 100%
+  display fixed
+  overflow hidden
+
+#scrollArea
+  margin-left 340px
+  width calc(100% - 340px)
+  overflow-x hidden
 </style>
