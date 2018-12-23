d063d19eca7ff3c33484a3cdd341cc25c46af48d
mkobayashime <m.kobayashi.me@gmail.com>
2018-12-23 20:40:30 +0900

    Add NpSection component

---
+++ components/NpSection.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .npSectionContainer
+    h2.fi.fi-up(data-emergence="hidden") {{section.title}}
+    p.fi.fi-up(data-emergence="hidden" v-for="p in section.ps") {{p.text}}
+</template>
+
+<script>
+import emergence from 'emergence.js'
+export default {
+  name: 'NpSection',
+  props: {
+    section: {
+      type: Object,
+      default: function() {
+        return {
+          title: 'This is Title',
+          ps: [{ text: 'This is p1' }]
+        }
+      }
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+h2
+  margin 100px 0 50px
+  font-size 40px
+p
+  margin 20px 0
+  font-size 16px
+</style>
